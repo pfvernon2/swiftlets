f9144ca279e2b45bed170c8060cfb969b26415ab
Frank Vernon <frank@ridefare.com>
2016-05-16 19:03:13 -0600

    Adding circle option for UIImageView

--- iOSX/iOS/RoundedCornerView.swift
+++ iOSX/iOS/RoundedCornerView.swift
@@ -27,3 +27,38 @@ class RoundedCornerView: UIView {
     }
     
 }
+
+extension CGRect {
+    var center:CGPoint {
+        return CGPointMake(CGRectGetMidX(self), CGRectGetMidY(self));
+    }
+    
+    static func rectCenteredOn(center:CGPoint, radius:CGFloat) -> CGRect {
+        return CGRectMake(floor(center.x - radius), floor(center.y - radius), floor(radius*2.0), floor(radius*2.0))
+    }
+}
+
+class RoundImageView: UIImageView {
+    
+    func makeRound() {
+        let minDimension:CGFloat = min(self.bounds.width, self.bounds.height)
+        
+        let layer:CALayer = self.layer
+        layer.masksToBounds = true
+        layer.cornerRadius = minDimension/2.0
+        
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.blackColor().CGColor
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.makeRound()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        makeRound()
+    }
+
+}
