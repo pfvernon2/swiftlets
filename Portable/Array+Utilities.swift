de4039c725c4b55791324fcf57f8bdcb3f70f0f4
Frank Vernon <frank@ridefare.com>
2016-06-05 13:13:10 -0600

    Adding contribution from David Vernon for Array move method.

---
+++ Portable/Array+Utilities.swift
@@ -0,0 +1,38 @@
+//
+//  Array+DHV.swift
+//  WebResearch
+//
+//  Created by David Vernon on 6/5/16.
+//  Copyright © 2016 David Vernon. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    
+    /**
+     Move an element of an array to another index position.
+     
+     - parameters:
+         - fromIndex: The index of an element in the array.
+         - toIndex: The index of a position in the array to which to move the element
+     */
+
+    public mutating func moveElement(fromIndex fromIndex:Int, toIndex:Int) {
+        self.insert(removeAtIndex(fromIndex), atIndex: toIndex)
+    }
+    
+    /**
+     Return a copy of an array with an element moved to another index position.
+     
+     - parameters:
+         - fromIndex: The index of an element in the array.
+         - toIndex: The index of a position in the array to which to move the element
+     */
+
+    public func arrayWithElementMoved(fromIndex fromIdex:Int, toIndex:Int) -> Array {
+        var result = self
+        result.moveElement(fromIndex: fromIdex, toIndex: toIndex)
+        return result
+    }
+}
