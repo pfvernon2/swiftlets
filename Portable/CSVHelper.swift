d2c940d26fd8f7bbec9ae8831f3cbd37acde96ad
Frank Vernon <frank@cyberdev.com>
2017-07-15 12:57:29 -0600

    Updating CSVHelper for Swift 4

--- Portable/CSVHelper.swift
+++ Portable/CSVHelper.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+fileprivate let fieldDelimiter:Character = ","
+fileprivate let recordDelimiter:Character = "\r\n"
+fileprivate let bareReturn:Character = "\r"
+fileprivate let bareLinefeed:Character = "\n"
+fileprivate let quoteCharacter:Character = "\""
+fileprivate let escapeSequence:String = "\"\""
+fileprivate let quoteSequence:String = String("\"")
+fileprivate let fieldDelimiterSequence:String = String(",")
+
+///RFC 4180 compliant CSV parser/writer
 class CSVHelper: NSObject {
-	
-	let delimiter:Character = ","
 	
 	func read(contentsOfURL url: URL, useEncoding encoding: String.Encoding = String.Encoding.utf8) -> [[String]] {
 		let characterData:String = try! String(contentsOfFile: url.path, encoding: encoding)
@@ -21,31 +29,33 @@ class CSVHelper: NSObject {
 		var testRecordEnd:Bool = false
 		var field:String = ""
 		var record:[String] = []
-		for current:Character in characterData.characters {
-			if testEscaped && current != "\"" {
+        for current:Character in characterData.characters {
+			if testEscaped && current != quoteCharacter {
 				testEscaped = false
 				quoted = false
 			}
 			
 			//check for escape sequence start
-			if current == "\"" && quoted && !testEscaped {
+			if current == quoteCharacter && quoted && !testEscaped {
 				testEscaped = true
 				continue
 			}
 				
             //check for quote sequence start
-			else if current == "\"" && !testEscaped {
+			else if current == quoteCharacter && !testEscaped {
 				quoted = !quoted
 			}
 				
             //if not quoted check for record delimiter(s)
             // supporting bare CR & LF, not required by RFC4180 but common
-			else if !quoted && (current == "\r" || current == "\n") {
+			else if !quoted && (current == bareReturn || current == bareLinefeed) {
 				testRecordEnd = true
-				continue
+                continue
 			}
                 
-			else if !quoted && (current == "\r\n" || testRecordEnd) {
+            //if outside of quoted section and at end of record then: add last field to record,
+            // add record to table, prepare to parse next record
+			else if !quoted && (current == recordDelimiter || testRecordEnd) {
 				record.append(field)
 				table.append(record)
 				record = []
@@ -56,7 +66,7 @@ class CSVHelper: NSObject {
 			}
 				
             //if not quoted check for field delimiter
-			else if !quoted && current == self.delimiter {
+			else if !quoted && current == fieldDelimiter {
 				record.append(field)
 				field = ""
 			}
@@ -72,29 +82,69 @@ class CSVHelper: NSObject {
 		
 		return table
 	}
-	
+    
 	func write(_ table: [[String]], toFile url: URL, useEncoding encoding: String.Encoding = String.Encoding.utf8) {
-		var output:String = String()
-		
-		for (_, record) in table.enumerated() {
-			var row:String = ""
-			for (_, field) in record.enumerated() {
-				let escaped = field.replacingOccurrences(of: "\"", with: "\"\"", options: NSString.CompareOptions.literal, range: nil)
-				let quoted = "\"" + escaped + "\""
-				
-				if row.characters.count > 0 {
-					row += ","
-				}
-				row += quoted
-			}
-			
-			row += "\r\n"
-			output += row
-		}
-		
-		do {
-			try output.write(to: url, atomically: true, encoding: encoding)
-		} catch _ {
-		}
+        guard let stream = OutputStream(url: url, append: true) else {
+            return
+        }
+        stream.open()
+        
+        defer {
+            stream.close()
+        }
+        
+        table.forEach { (record) in
+            stream.write(encode(record: record))
+        }
 	}
+    
+    fileprivate func encode(record: [String]) -> String {
+        let escapedRecord:[String] = record.map { (field) -> String in
+            guard field.contains(quoteCharacter) else {
+                return field
+            }
+            
+            let escapedField = field.replacingOccurrences(of: quoteSequence, with: escapeSequence, options: NSString.CompareOptions.literal, range: nil)
+            return quoteSequence + escapedField + quoteSequence
+        }
+        return escapedRecord.joined(separator: fieldDelimiterSequence)
+    }
+}
+
+fileprivate extension OutputStream {
+    
+    /// Write `String` to `OutputStream`
+    ///
+    /// - parameter string:                The `String` to write.
+    /// - parameter encoding:              The `String.Encoding` to use when writing the string. This will default to `.utf8`.
+    /// - parameter allowLossyConversion:  Whether to permit lossy conversion when writing the string. Defaults to `false`.
+    ///
+    /// - returns:                         Return total number of bytes written upon success. Return `-1` upon failure.
+    
+    @discardableResult func write(_ string: String, encoding: String.Encoding = .utf8, allowLossyConversion: Bool = false) -> Int {
+        
+        if let data = string.data(using: encoding, allowLossyConversion: allowLossyConversion) {
+            return data.withUnsafeBytes { (bytes: UnsafePointer<UInt8>) -> Int in
+                var pointer = bytes
+                var bytesRemaining = data.count
+                var totalBytesWritten = 0
+                
+                while bytesRemaining > 0 {
+                    let bytesWritten = self.write(pointer, maxLength: bytesRemaining)
+                    if bytesWritten < 0 {
+                        return -1
+                    }
+                    
+                    bytesRemaining -= bytesWritten
+                    pointer += bytesWritten
+                    totalBytesWritten += bytesWritten
+                }
+                
+                return totalBytesWritten
+            }
+        }
+        
+        return -1
+    }
+    
 }
---
+++ Portable/LineReader.swift
@@ -0,0 +1,74 @@
+//
+//  LineReader.swift
+//  swiftlets
+//
+//  Created by Frank Vernon on 7/7/17.
+//  Copyright © 2017 Frank Vernon. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Utility class and associated Sequence extension to read file delimited by line ending.
+ 
+ - note: This is an old school C style ASCII file input technique. It is not Unicode aware
+ and as such may be of limited value. It is intened for processing structured files
+ containing ASCII endoded data such as simple log files, for example.
+ 
+ ```
+ guard let lineReader:LineReader = LineReader(url: url) else {
+     return
+ }
+ 
+ for nextLine in lineReader {
+     //process nextLine
+ }
+ ```
+ */
+class LineReader {
+    fileprivate let file: UnsafeMutablePointer<FILE>
+    
+    init?(path: String) {
+        guard let fileRef = fopen(path, "r") else {
+            return nil
+        }
+        
+        self.file = fileRef
+    }
+    
+    init?(url: URL) {
+        guard url.isFileURL,
+            let fileRef = fopen(url.path, "r") else {
+            return nil
+        }
+        
+        self.file = fileRef
+    }
+    
+    var nextLine: String? {
+        var line:UnsafeMutablePointer<CChar>? = nil
+        defer {
+            free(line)
+        }
+        
+        var linecap:Int = 0
+        guard getline(&line, &linecap, file) > 0, line != nil else {
+            return nil
+        }
+        
+        //force unwrap protected by guard above
+        return String(cString: line!)
+    }
+    
+    deinit {
+        fclose(file)
+    }
+}
+
+extension LineReader: Sequence {
+    func  makeIterator() -> AnyIterator<String> {
+        return AnyIterator<String> {
+            return self.nextLine
+        }
+    }
+}
